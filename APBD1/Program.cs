a0113a48e6499641d047a3a9f9a063e8f41ea601
Konrad Dominowski <zehkan@gmail.com>
2026-03-18 18:28:50 +0100

    Add new user functionality

--- APBD1/Program.cs
+++ APBD1/Program.cs
@@ -1,16 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Runtime.InteropServices.JavaScript;
 using APBD1.Equipment;
+using APBD1.User;
+using APBD1.Service;
 using OperatingSystem = APBD1.Equipment.OperatingSystem;
 
 var laptop = new Laptop("lap", 17, 10000, OperatingSystem.Linux);
 var laptop1 = new Laptop("lap", 17, 10000, OperatingSystem.Linux);
 
-Console.WriteLine(laptop.Id);
-Console.WriteLine(laptop1.Id);
 
 var date = DateTime.Now;
 var date2 = DateTime.Now.AddDays(5);
 
-Console.WriteLine(date.Day);
+var user1 = new Student("Konrad", "Dominowski");
+var user2 = new Employee("Andrzej", "Kowalski");
+var user3 = new Employee("Andrzej", "Kowalski");
+
+Console.WriteLine("Witaj w systemie zarządzania wypożyczalnią.");
+
+Service.ShowMenu();
--- APBD1/User/User.cs
+++ APBD1/User/User.cs
@@ -1,16 +1,62 @@
-﻿namespace APBD1.User;
+﻿using System.Reflection.Metadata;
 
-public class User
+namespace APBD1.User;
+
+public abstract class User
 {
     public static int MaxId = 0;
     public int Id { get; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public abstract int MaxRentalsAvailable { get; }
+    public static List<User> Users = [];
 
-    public User(string firstName, string lastName)
+
+    protected User(string firstName, string lastName)
     {
         Id = ++MaxId;
         FirstName = firstName;
         LastName = lastName;
+        Users.Add(this);
+    }
+
+    public static User CreateUser()
+    {
+        var firstName = GetString("Imię");
+        var lastName = GetString("Nazwisko");
+
+        while (true)
+        {
+            Console.WriteLine("Wybierz opcję:");
+            Console.WriteLine("1. Student");
+            Console.WriteLine("2. Pracownik");
+
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                switch (number)
+                {
+                    case 1:
+                        return new Student(firstName, lastName);
+                    case 2:
+                        return new Employee(firstName, lastName);
+                    default:
+                        continue;
+                }
+            }
+            
+            Console.WriteLine("Błędna liczba!");
+        }
+    }
+
+    private static string GetString(string name)
+    {
+        string? input;
+        do
+        {
+            Console.Write($"{name}: ");
+            input = Console.ReadLine();
+        } while (string.IsNullOrWhiteSpace(input));
+
+        return input!;
     }
 }
